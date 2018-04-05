cfe9deefeb1c1a0f52a599d2dbe09606bf145d54
Nikola Chetelyazov <avenue9977@gmail.com>
2018-04-05 16:48:21 +0300

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/*jshint esversion: 6 */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    devtool: 'inline-source-maps',
+    // devServer: {
+    //     contentBase: './dist'
+    // },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Code Breaker',
+            template: 'src/index.html',
+            favicon: 'src/favicon.png'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
+    },
+    mode: "production"
+};
